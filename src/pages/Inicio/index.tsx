0b853ac0a85d9cb275e195513773f25a0f5ce740
Victor Martins <vitor_jhoul@hotmail.com>
2022-07-03 16:11:52 -0300

    Refatorando partes do projeto e utilizando React router Dom

--- src/pages/Inicio/index.tsx
+++ src/pages/Inicio/index.tsx
@@ -1,5 +1,6 @@
 import cardapio from 'data/cardapio.json';
 import styles from './Inicio.module.scss';
+import stylesTema from 'styles/Tema.module.scss'
 
 export default function Inicio() {
   let pratosRecomendados = [...cardapio];
@@ -7,8 +8,8 @@ export default function Inicio() {
     .sort(() => 0.5 - Math.random())
     .splice(0, 3);
   return (
-    <section>
-      <h3 className={styles.titulo}>Recomendaçoes da cozinha</h3>
+    <section>      
+      <h3 className={stylesTema.titulo}>Recomendaçoes da cozinha</h3>
       <div className={styles.recomendados}>
         {pratosRecomendados.map((item) => (
           <div key={item.id} className={styles.recomendado}>
